5c264212a3c376da9bb09857c7cf57839760f7ac
mateusDbarros <mb685212@gmail.com>
2025-01-31 19:26:41 -0300

    Atualizando o codigo

---
+++ src/main/java/mb/projeto/ONG/Ongs/Service/OngService.java
@@ -0,0 +1,61 @@
+package mb.projeto.ONG.Ongs.Service;
+
+import mb.projeto.ONG.Ongs.Model.Ong;
+import mb.projeto.ONG.Ongs.Repository.OngRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OngService {
+
+    private final OngRepository repository;
+
+    public OngService(OngRepository repository) {
+        this.repository = repository;
+    }
+
+    // Regras de negocio
+
+    // Metodo para checagem de excessões
+    public boolean check(Ong ong) {
+        if (ong.getNome() == null || ong.getNome().isEmpty())
+            throw new IllegalStateException("Por favor insira um NOME valido para a ong.");
+        if (ong.getCategoria() == null)
+            throw new IllegalStateException("Por favor insira uma CATEGORIA para a ong.");
+        if (ong.getSaldo() < 0)
+            throw new IllegalStateException("SALDO não pode ser menor que 0.");
+        return true;
+    }
+
+    // Metodo de cadastro
+    public void novaOng(Ong ong) {
+        if (check(ong))
+            repository.novaOng(ong);
+    }
+
+    // Metodo de vizualizar todos
+    public List<Ong> ListarOngs() {
+        return repository.listarOngs();
+    }
+
+    // Metodo de procurar por id
+    public Ong findById(long id) {
+        if (!repository.existe(id))
+            throw new IllegalStateException("ID não encontrado para pesquisa.");
+        return repository.findById(id);
+    }
+
+    // Metodo de atualização
+    public void updateOng(Ong ong) {
+        if (check(ong))
+            repository.updateOng(ong);
+    }
+
+    // Metodo de exclusao
+    public void deleteOng(long id) {
+        if (!repository.existe(id))
+            throw new IllegalStateException("ID não encontrado para exclusão");
+        repository.deleteOng(id);
+    }
+}
